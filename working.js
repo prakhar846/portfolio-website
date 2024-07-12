3b1531ff47e2e165ec8d76f0d2a777e110cc0f02
prakhar846 <59389050+prakhar846@users.noreply.github.com>
2024-07-12 22:25:40 +0530

    Add files via upload

---
+++ working.js
@@ -0,0 +1,10 @@
+
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+
+    var typed = new Typed('#element', {
+        strings: ['Web Developer.', 'Problem Solver.', 'Consultant.'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true,
+        showCursor: false
+    });
